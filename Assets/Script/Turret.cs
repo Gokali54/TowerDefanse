ad4eee1979e75ea6de79f7385ee7a012786ba649
DESKTOP-M07MIJN\MONSTER <gokali.anonim@gmail.com>
2022-09-27 23:38:52 +0300

    kursun ayarları yapıldı.

--- Assets/Script/Turret.cs
+++ Assets/Script/Turret.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    public GameObject silahin_kafasi;
+    public Transform silahin_kafasi, namlu;
     // radarý sabitten deðil silahin içine ekledikten sonra sc ye ekle 
     public GameObject radar;
-    public GameObject namlu, kursun;
-   public List<Transform> hedef;
-  public  Transform suanki_hedef;
-    public Transform radar_transfor;
+    public GameObject kursun;
+    public ParticleSystem alev_effect;
+    public List<Transform> hedef;
+    public  Transform suanki_hedef;
+    public float ates_etme_araliði,kursun_hiz;
+    public float gelecek_atesi_suresi=0;
+    bool ates = false;
 
     public int silahin_leveli;
-    public float ates_etme_araliði;
-    float gelecek_atesi_suresi;
-    bool ates = false;
     public int gelisim_maliyeti;
     public int yikim_geliri;
 
@@ -25,6 +25,7 @@ public class Turret : MonoBehaviour
 
     public void ilk_temas(Collider other)
     {
+      
         hedef.Add(other.gameObject.transform);
         suanki_hedef = hedef[0];
         ates = true;
@@ -41,6 +42,10 @@ public class Turret : MonoBehaviour
         {
             suanki_hedef = null;
             ates = false;
+            if (alev_effect != null)
+            {
+                alev_effect.Stop();
+            }
         }
     }
 
@@ -55,7 +60,7 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     private void OnMouseDown()
     {
-        Debug.Log("onclik");
+        
         GameObject[] tümradarlar;
         tümradarlar = GameObject.FindGameObjectsWithTag("radar");
         foreach (var item in tümradarlar)
@@ -69,6 +74,69 @@ public class Turret : MonoBehaviour
 
     void Update()
     {
-        silahin_kafasi.transform.Rotate(0, 1, 0);
+        if (suanki_hedef != null)
+        {
+            silahin_kafasi.LookAt(suanki_hedef);
+            
+        }
+        else
+        {
+        silahin_kafasi.Rotate(0, 1, 0);
+        }
+        if (ates == true)
+        {            
+            if (Time.time > gelecek_atesi_suresi)
+            {
+             
+                ateset();
+                gelecek_atesi_suresi = Time.time + ates_etme_araliði;
+                if (alev_effect != null)
+                {                 
+                    alev_effect.Play();
+                }
+            }
+        }
+
+        
     }
+
+    public void ateset()
+    {
+       
+        if (suanki_hedef == null && ates == true)
+        {
+            for (int i = 0; i < hedef.Count; i++)
+            {
+                if (hedef[i] == null)
+                {
+                    hedef.RemoveAt(i);
+                }
+            }
+
+            if (hedef.Count > 0)
+            {
+                suanki_hedef = hedef[0];
+            }
+            else
+            {
+                ates = false;
+                if (alev_effect != null)
+                {
+                    Debug.Log("d3eðil");
+                    alev_effect.Stop();
+                }
+            }
+
+        }
+        kursun_uret();
+    }
+
+    public void kursun_uret()
+    {
+        GameObject yeni_kursun = Instantiate(kursun, namlu.position, Quaternion.identity);
+        yeni_kursun.transform.LookAt(suanki_hedef);
+        yeni_kursun.GetComponent<Rigidbody>().velocity = namlu.forward * kursun_hiz;
+        Destroy(yeni_kursun, 1f);
+    }
+
 }
